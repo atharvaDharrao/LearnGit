58a8ed895fc49a97ed10afdeceb08ade68ac94fb
Atharva Dharrao <atharvadharrao11@gmail.com>
2026-01-02 23:37:54 +0530

    Updated Docs

--- hello.py
+++ hello.py
@@ -6,7 +6,11 @@ print("THIRD LINE")
 
 """
 1. git init
-2. git add . (So you are telling Git:“I want all these changes to be included in the next commit.”)
+2. git add . ==> (So you are telling Git:“I want all these changes to be included in the next commit.”)
 3. git commit -m "Initial Commit"
-4. git branch -M main
+4. git branch -M main ==> (Renames the branch to main, -M means force rename) 
+
+
+5. git branch ==> (to check the current branch)
+6. git status ==> (to check status : modified or not)
 """
