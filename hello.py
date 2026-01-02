1c2c10a797966dfefbf939cd6b42559106284ded
Atharva Dharrao <atharvadharrao11@gmail.com>
2026-01-02 23:32:42 +0530

    Added Docs

--- hello.py
+++ hello.py
@@ -3,3 +3,10 @@ print("hello world")
 print("byr world")
 
 print("THIRD LINE")
+
+"""
+1. git init
+2. git add . (So you are telling Git:“I want all these changes to be included in the next commit.”)
+3. git commit -m "Initial Commit"
+4. git branch -M main
+"""
