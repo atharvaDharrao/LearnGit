81d8d97636f07a8370d0aecf2e47bc3e9a34c1a2
Atharva Dharrao <atharvadharrao11@gmail.com>
2026-01-03 00:30:06 +0530

    Last commit in main branch

--- hello.py
+++ hello.py
@@ -11,6 +11,18 @@ print("THIRD LINE")
 4. git branch -M main ==> (Renames the branch to main, -M means force rename) 
 
 
-5. git branch ==> (to check the current branch)
+5. git branch ==> (to check all the branches in remote repository)
+**note**:
+git branch -r: Lists all remote-tracking branches (the branches on the remote repository, like GitHub or GitLab) [1].
+git branch -a: Lists both local and remote-tracking branches [1]. 
+
 6. git status ==> (to check status : modified or not)
+7. git log --oneline ==> (to check all the commits inside a branch)
+
+
+
+
+[1] What is branch in Git ?
+==> A branch is an independent line of development.
+==> A separate workspace where you can work without breaking the main code.
 """
